1f2395406de4885116f9ea52527e4036ab84c2e0
박원빈 <harry21@kakao.com>
2024-05-01 18:47:22 +0900

    (add) render movies

--- js/main/index.js
+++ js/main/index.js
@@ -1,3 +1,4 @@
+import MoviesInfo from "./components/MoviesInfo.js";
 import useMovieData from "./hook/useMovieData.js";
 
 const { getPopularMovies, getTopRatedMovies, getUpcomingMovies } =
@@ -8,3 +9,15 @@ const [popularMovies, topRatedMovies, upcomingMovies] = await Promise.all([
   getTopRatedMovies(1),
   getUpcomingMovies(1),
 ]);
+
+const topRatedMovieElements = MoviesInfo(topRatedMovies);
+const popularMovieElements = MoviesInfo(popularMovies);
+const upcomingMovieElements = MoviesInfo(upcomingMovies);
+
+const topRatedMoviesWrap = document.getElementById("top-rated-movies-wrap");
+const popularMoviesWrap = document.getElementById("popular-movies-wrap");
+const upcomingMoviesWrap = document.getElementById("upcoming-movies-wrap");
+
+topRatedMoviesWrap.innerHTML = topRatedMovieElements;
+popularMoviesWrap.innerHTML = popularMovieElements;
+upcomingMoviesWrap.innerHTML = upcomingMovieElements;

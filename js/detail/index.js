dab2d08d4af474f00ccad4fe8daf8580df1706a0
박원빈 <harry21@kakao.com>
2024-05-03 13:33:59 +0900

    (add) useMovieDetailData: trailer data

--- js/detail/index.js
+++ js/detail/index.js
@@ -1,11 +1,14 @@
 import useMovieDetailData from "./hook/useMovieDetailData.js";
 
-const { getActorsData, getWatchProvidersData } = useMovieDetailData();
+const { getActorsData, getWatchProvidersData, getVideoData } =
+  useMovieDetailData();
 
 const movieId = JSON.parse(sessionStorage.getItem("movie")).id;
 
 const actors = await getActorsData(movieId);
 const providers = await getWatchProvidersData(movieId);
+const trailer = await getVideoData(movieId);
 
 console.log(actors);
 console.log(providers);
+console.log(trailer);

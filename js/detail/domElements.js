1db338b7b1771663cfb86282a4fab0aa586975ed
mangmuse <mangmangmuse@gmail.com>
2024-05-03 13:19:27 +0900

    (add) password verification feature

--- js/detail/domElements.js
+++ js/detail/domElements.js
@@ -2,3 +2,11 @@ export const reviewsWrapper = document.querySelector("#reviews-wrapper");
 export const reviewContainer = document.querySelector("#review-list");
 export const reviewForm = document.querySelector("#review-form");
 export const reviewCnt = document.querySelector("#review-cnt");
+export const passwordModal = document.querySelector("#password-modal");
+export const passwordModalInput = document.querySelector(
+  "#password-modal-input"
+);
+
+export const passwordModalCancelBtn = document.querySelector(
+  "#password-modal-cancel-btn"
+);

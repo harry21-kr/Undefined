94cb582734c0881628b7d359f60dfa6bfc508f33
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-05-04 17:42:44 +0900

    (add) display trailer

--- js/detail/index.js
+++ js/detail/index.js
@@ -1,19 +1,24 @@
 import useMovieDetailData from "./hook/useMovieDetailData.js";
 import displayActors from "./displayActors.js";
+import displayTrailer from "./displayTrailer.js";
 
 const { getActorsData, getWatchProvidersData, getVideoData } =
   useMovieDetailData();
 
 const movieId = JSON.parse(sessionStorage.getItem("movie")).id;
 
-const [actors, providers, trailer] = await Promise.all([
+// const [actors, providers, trailer] = await Promise.all([
+const [actors, trailer] = await Promise.all([
   getActorsData(movieId),
   // getWatchProvidersData(movieId),
-  // getVideoData(movieId),
+  getVideoData(movieId),
 ]);
 
 const actorsWrap = document.getElementById("actor-row");
+const trailerWrap = document.getElementById("trailer-wrapper");
 
 const actorElements = displayActors(actors);
+const trailerElement = displayTrailer(trailer);
 
 actorsWrap.innerHTML = actorElements;
+trailerWrap.innerHTML = trailerElement;

854955b8b673070bfbb1ae838a300acfa6859a59
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-05-06 01:59:42 +0900

    (fix) provider empty check

--- js/detail/displayProviders.js
+++ js/detail/displayProviders.js
@@ -1,6 +1,7 @@
 import { TMDB_IMAGE_URL } from "../config/constants/index.js";
 
 export default function displayProviders(data) {
+  console.log(data);
   if (data) {
     const providerElements = data
       .map((provider) => {
@@ -11,5 +12,7 @@ export default function displayProviders(data) {
       .reduce((acc, cur) => acc + cur, []);
 
     return providerElements;
+  } else {
+    return "아직 한국에서 시청 가능한 플랫폼이 없습니다.";
   }
 }

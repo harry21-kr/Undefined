22a8edef55f1af8e98a1e1d54cf0e36ea3161569
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-05-05 16:27:49 +0900

    (fix) review validation

--- js/detail/handleAdd.js
+++ js/detail/handleAdd.js
@@ -6,49 +6,48 @@ export const handleSubmit = (e) => {
   const reviewInputs = document.querySelectorAll(".review-input");
   e.preventDefault();
 
-  for (const tag of reviewInputs.values()) {
-    const value = tag.value.trim();
+  const [userName, userPassword, userReview] = [
+    reviewInputs[0].value.trim(),
+    reviewInputs[1].value.trim(),
+    reviewInputs[2].value.trim(),
+  ];
 
-    if (tag.id === "username") {
-      if (value.length < 2) {
-        errorMessage = "작성자를 두 글자 이상 입력해 주세요.";
-        break;
-      } else {
-        review[tag.id] = value;
-      }
-    }
-    if (tag.id === "password") {
-      if (value.length < 8 || !/[!@#$%^&*(),.?":{}|<>]/.test(value)) {
-        errorMessage = "비밀번호를 특수문자를 포함한 8자 이상 입력해 주세요.";
-        break;
-      } else {
-        review[tag.id] = value;
-      }
-    }
-    if (tag.id === "review") {
-      if (value.length < 10) {
-        errorMessage = "리뷰를 10자 이상 작성해 주세요.";
-        break;
-      } else {
-        review[tag.id] = value;
-      }
-    }
-  }
-
-  if (!errorMessage) {
-    const date = new Date();
-    const submittedAt = `${date.getFullYear()}-${
-      date.getMonth() + 1
-    }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
-    review = { ...review, submittedAt, movieId: "/" };
-    const key = date.getTime();
-
-    localStorage.setItem(key, JSON.stringify(review));
-    addReview(review, key);
-    reviewInputs.forEach((e) => (e.value = ""));
-  } else {
+  if (userName.length < 2) {
+    errorMessage = "작성자를 두 글자 이상 입력해 주세요.";
     errorMsgPar.innerHTML = errorMessage;
+    return;
+  } else {
+    review["username"] = userName;
   }
+
+  if (userPassword.length < 8 || !/[!@#$%^&*(),.?":{}|<>]/.test(userPassword)) {
+    errorMessage = "비밀번호를 특수문자를 포함한 8자 이상 입력해 주세요.";
+    errorMsgPar.innerHTML = errorMessage;
+    return;
+  } else {
+    review["password"] = userPassword;
+  }
+
+  if (userReview.length < 10) {
+    errorMessage = "리뷰를 10자 이상 작성해 주세요.";
+    errorMsgPar.innerHTML = errorMessage;
+    return;
+  } else {
+    review["review"] = userReview;
+  }
+
+  const date = new Date();
+  const submittedAt = `${date.getFullYear()}-${
+    date.getMonth() + 1
+  }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+  review = { ...review, submittedAt, movieId: "/" };
+  const key = date.getTime();
+
+  localStorage.setItem(key, JSON.stringify(review));
+  addReview(review, key);
+
+  reviewInputs.forEach((e) => (e.value = ""));
+  errorMsgPar.innerHTML = "";
 };
 
 function addReview(review, key) {

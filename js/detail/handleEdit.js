a9bac0a9a2db61c63efff17629521599f4968b94
mangmuse <mangmangmuse@gmail.com>
2024-05-05 18:24:11 +0900

    (add) reassign edited value to localstorage

--- js/detail/handleEdit.js
+++ js/detail/handleEdit.js
@@ -12,25 +12,27 @@ const editReview = (localStorageKey) => {
   );
   const paragraph = reviewContentBox.querySelector("#review-display");
   const textContent = paragraph.innerText;
-
-  displayEditInput(reviewContentBox, textContent);
+  displayEditInput(reviewContentBox, textContent, localStorageKey);
 };
 
-const displayEditInput = (reviewContentBox, textContent) => {
+const displayEditInput = (reviewContentBox, textContent, localStorageKey) => {
   reviewContentBox.innerHTML = `<textarea id="edit-review-input">${textContent}</textarea>
   <button id="edit-review-confirm-btn">확인</button>
   `;
 
-  setupEditConfirm(reviewContentBox);
+  setupEditConfirm(reviewContentBox, localStorageKey);
 };
 
-const setupEditConfirm = (reviewContentBox) => {
+const setupEditConfirm = (reviewContentBox, localStorageKey) => {
   const handleEditConfirm = () => {
     const editReviewInput = document.querySelector("#edit-review-input");
     const editedReview = editReviewInput.value;
     reviewContentBox.innerHTML = `
     <p id="review-display">${editedReview}</p>
 `;
+    const review = JSON.parse(localStorage.getItem(localStorageKey));
+    review.review = editedReview;
+    localStorage.setItem(localStorageKey, JSON.stringify(review));
   };
   document
     .querySelector("#edit-review-confirm-btn")

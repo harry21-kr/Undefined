18a2d909bc45d9fb52f26be2ea487e944296e979
박원빈 <harry21@kakao.com>
2024-05-07 17:16:09 +0900

    (design) add simiar movie layout

--- js/detail/displaySimilarMovies.js
+++ js/detail/displaySimilarMovies.js
@@ -5,8 +5,10 @@ export default function displaySimilarMovies(data) {
     const similarMovieElements = data
       .map((similarMovie) => {
         return `
+          <div>
             <img id="similar-movie-img" src="${TMDB_IMAGE_URL}${similarMovie.poster_path}" />
             <p id="similar-movie-title">${similarMovie.title}</p>
+          </div>
           `;
       })
       .reduce((acc, cur) => acc + cur, []);
